693daf596c275662d5ce8a38a7b3cb8ae6aab55d
Vagner Bom Jesus <vagneripg@gmail.com>
2019-03-18 23:45:06 +0000

    criar interface Dos show button Despesa e Receita

---
+++ app/src/main/java/pt/vagner/myfinances/NovaDespesa.java
@@ -0,0 +1,26 @@
+package pt.vagner.myfinances;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+
+public class NovaDespesa extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nova_despesa);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+}

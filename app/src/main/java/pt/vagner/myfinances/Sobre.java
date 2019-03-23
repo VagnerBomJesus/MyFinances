25d8d7a2c47713216ec3e6a8fee2963e19403178
Vagner Bom Jesus <vagneripg@gmail.com>
2019-03-23 00:24:31 +0000

    commit finalizar o intente sobre

--- app/src/main/java/pt/vagner/myfinances/Sobre.java
+++ app/src/main/java/pt/vagner/myfinances/Sobre.java
@@ -1,8 +1,14 @@
 package pt.vagner.myfinances;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
 
 public class sobre extends AppCompatActivity {
 
@@ -10,5 +16,17 @@ public class sobre extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sobre);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
     }
+
 }

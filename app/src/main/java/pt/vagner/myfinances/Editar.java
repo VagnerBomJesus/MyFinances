59330d776d099884f6da65ad8344bb9d966082ca
Vagner Bom Jesus <vagneripg@gmail.com>
2019-04-02 16:32:48 +0100

    Commit data atual in Editar

--- app/src/main/java/pt/vagner/myfinances/Editar.java
+++ app/src/main/java/pt/vagner/myfinances/Editar.java
@@ -10,7 +10,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.Calendar;
 
 public class Editar extends AppCompatActivity {
 
@@ -23,6 +26,8 @@ public class Editar extends AppCompatActivity {
 
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        setDefaultDateToTextView();///visualizar data atual
     }
     public void guardar(View view){
         Toast.makeText(getApplicationContext(),"Guardaro Com sucesso",Toast.LENGTH_LONG).show();
@@ -31,6 +36,45 @@ public class Editar extends AppCompatActivity {
     }
     public void cancel(View view) { //Botão "cancelar"
         finish();
+    }
+
+    /**
+     * @return dia atual
+     */
+    private int getCurrentDay(){
+        Calendar c = Calendar.getInstance();
+        int dia = c.get(Calendar.DAY_OF_MONTH);
+        return dia;
+    }
+
+    /**
+     * @return mes atual
+     */
+    private int getCurrentMonth(){
+        Calendar c = Calendar.getInstance();
+        int month = c.get(Calendar.MONTH)+1;
+        return month;
+    }
+
+    /**
+     * @return ano atual
+     */
+    private int getCurrentYear(){
+        Calendar c = Calendar.getInstance();
+        int ano = c.get(Calendar.YEAR);
+        return ano;
+    }
+    /**
+     * coloca na textview da data a data atual dd/mm/yyyy
+     */
+    private void setDefaultDateToTextView(){
+        TextView textViewDate = (TextView) findViewById(R.id.textViewSelectedDateDespesa);
+
+        int dia = getCurrentDay();
+        int mes = getCurrentMonth();
+        int ano = getCurrentYear();
+
+        textViewDate.setText(""+dia+"/"+mes+"/"+ano);
     }
 
 }

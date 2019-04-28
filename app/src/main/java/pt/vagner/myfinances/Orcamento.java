5515a10e7b7b6da6fe4d45f5484821bd3aa021d6
Vagner Bom Jesus <vagneripg@gmail.com>
2019-04-28 01:11:43 +0100

    finalizar os  Setters and Getters

---
+++ app/src/main/java/pt/vagner/myfinances/Orcamento.java
@@ -0,0 +1,31 @@
+package pt.vagner.myfinances;
+
+public class Orcamento {
+    int id_orcamento;
+    double valor;
+
+    //Construtores
+    public Orcamento(){}
+
+    public Orcamento(int id_orcamento, double valor) {
+        this.id_orcamento = id_orcamento;
+        this.valor = valor;
+    }
+
+    //Setters and Getters
+    public int getId_orcamento() {
+        return id_orcamento;
+    }
+
+    public void setId_orcamento(int id_orcamento) {
+        this.id_orcamento = id_orcamento;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+}

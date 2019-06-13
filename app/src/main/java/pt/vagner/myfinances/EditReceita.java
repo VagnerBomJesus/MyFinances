f52bb74743b463dec359da07df8b15fda3f3b6a4
Vagner Bom Jesus <vagneripg@gmail.com>
2019-06-13 01:04:21 +0100

    Editar Receita

--- app/src/main/java/pt/vagner/myfinances/EditReceita.java
+++ app/src/main/java/pt/vagner/myfinances/EditReceita.java
@@ -1,41 +1,121 @@
 package pt.vagner.myfinances;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cursoradapter.widget.SimpleCursorAdapter;
 import androidx.fragment.app.DialogFragment;
+import androidx.loader.app.LoaderManager;
+import androidx.loader.content.Loader;
 
 import android.app.DatePickerDialog;
 import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.DatePicker;
 import android.widget.EditText;
+
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.android.material.snackbar.Snackbar;
 
 import java.util.Calendar;
 import java.util.Date;
 
-public class EditReceita extends AppCompatActivity implements DatePickerDialog.OnDateSetListener,DialogFragmentCategoria.ExampleDialogListener  {
+public class EditReceita extends AppCompatActivity implements DatePickerDialog.OnDateSetListener,DialogFragmentCategoria.ExampleDialogListener , LoaderManager.LoaderCallbacks<Cursor> {
 
     private EditText editTextValorReceita;
+    private Spinner spinnerCategoria;
+    private EditText editTextDescricao;
     private TextView textViewSelectedDate;
     private TextView textViewData;
-    private static final int CATEGORIES_CURSOR_LOADER_ID = 0;
 
+    private static final int CATEGORIES_CURSOR_LOADER_ID = 0;
+    private TipoReceita tipoReceita = null;
+
+    private boolean categoriasCarregadas = false;
+    private boolean categoriaAtualizada = false;
+
+    private Uri enderecoReceitaEditar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_receita);
-        editTextValorReceita = (EditText) findViewById(R.id.editTextDesignacaoDespesa);
-        textViewSelectedDate = (TextView) findViewById(R.id.textViewSelectedDate);
-        textViewData= (TextView) findViewById(R.id.textViewData);
+
+
+        editTextDescricao = (EditText) findViewById(R.id.editTextDesignacaoReceita);
+        spinnerCategoria = (Spinner) findViewById(R.id.spinnerCategoria);
+        editTextValorReceita = (EditText) findViewById(R.id.editTextValorReceita);
+       // textViewSelectedDate = (TextView) findViewById(R.id.textViewSelectedDate);
+        //textViewData= (TextView) findViewById(R.id.textViewData);
+
+        getSupportLoaderManager().initLoader(CATEGORIES_CURSOR_LOADER_ID, null, this);
+
+        Intent intent = getIntent();
+
+        long idReceita = intent.getLongExtra(ListarTodoTipoReceitaActivity.ID_RECEITA, -1);
+
+        if(idReceita == -1){
+            Toast.makeText(this, "Erro: não foi possiveli ler o registo", Toast.LENGTH_LONG).show();
+            finish();
+            return;
+        }
+
+        enderecoReceitaEditar = Uri.withAppendedPath(FinanceContentProvider.ENDERECO_TIPO_RECEITA, String.valueOf(idReceita));
+
+        Cursor cursor = getContentResolver().query(enderecoReceitaEditar, BdTabelaTipoReceita.TODAS_COLUNAS, null, null, null);
+        if (!cursor.moveToNext()){
+            Toast.makeText(this, "Erro: não foi possivel ler o registo", Toast.LENGTH_LONG).show();
+            finish();
+            return;
+        }
+
+        tipoReceita = TipoReceita.fromCursor(cursor);
+        editTextDescricao.setText(tipoReceita.getDescricaoReceita());
+        editTextValorReceita.setText(String.valueOf(tipoReceita.getValor()));
 
 
 
+        actualizaCategoriaSelecionada();
         setDefaultDateToTextView();///visualizar data atual
     }
 
+    private void actualizaCategoriaSelecionada() {
+        if (!categoriasCarregadas) return;
+        if (categoriaAtualizada) return;
+
+        for (int i = 0; i < spinnerCategoria.getCount(); i++) {
+            if (spinnerCategoria.getItemIdAtPosition(i) == tipoReceita.getCategoria()) {
+                spinnerCategoria.setSelection(i);
+                break;
+            }
+        }
+
+        categoriaAtualizada = true;
+    }
+
+    @Override
+    protected void onResume() {
+        getSupportLoaderManager().restartLoader(CATEGORIES_CURSOR_LOADER_ID, null, this);
+
+        super.onResume();
+    }
+    private void mostraCategoriasSpinner(Cursor cursorCategorias) {
+        SimpleCursorAdapter adaptadorCategorias1 = new SimpleCursorAdapter(
+                this,
+                android.R.layout.simple_list_item_1,
+                cursorCategorias,
+                new String[]{BdTabelaCategoria.CAMPO_DESCRICAO},
+                new int[]{android.R.id.text1}
+        );
+        spinnerCategoria.setAdapter(adaptadorCategorias1);
+
+    }
     public void atualizarReceita(View view) {
         EditText editTextValorReceita = (EditText) findViewById(R.id.editTextValorReceita);
         String mensagem = editTextValorReceita.getText().toString();
@@ -64,15 +144,50 @@ public class EditReceita extends AppCompatActivity implements DatePickerDialog.O
             editTextValorReceita.requestFocus();
             return;
         }
-        Date data = new Date();
+        long idCategoria = spinnerCategoria.getSelectedItemId();
+
+        //Guaradara dados
+        tipoReceita.setDescricaoReceita(designacao);
+        tipoReceita.setCategoria(idCategoria);
+        tipoReceita.setValor(valor);
+
+        try {
+            getContentResolver().update(enderecoReceitaEditar, tipoReceita.getContentValues(),
+            null, null);
+
+            Toast.makeText(this, "Registo Guaradado com Sucesso", Toast.LENGTH_LONG).show();
+            finish();
+
+        }catch (Exception e ){
+            Snackbar.make(
+                    editTextDesignacaoReceita,"Erro: não foi possível guaradar o registo",
+                    Snackbar.LENGTH_LONG
+            ).show();
+            e.printStackTrace();
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         Intent intent = new Intent(this, ListarTodoTipoReceitaActivity.class);
 
-        intent.putExtra(DefinicoesApp.Designacao, designacao);
-
-        startActivity(intent);
-
-        intent.putExtra(DefinicoesApp.MENSAGEM, mensagem);
-        intent.putExtra(DefinicoesApp.DATA, data);
+//        //intent.putExtra(DefinicoesApp.Designacao, designacao);
+//
+//        startActivity(intent);
+//
+//        intent.putExtra(DefinicoesApp.MENSAGEM, mensagem);
+//        intent.putExtra(DefinicoesApp.DATA, data);
 
         startActivity(intent);
     }
@@ -158,4 +273,26 @@ public class EditReceita extends AppCompatActivity implements DatePickerDialog.O
     }
 
 
+    @NonNull
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, @Nullable Bundle args) {
+        androidx.loader.content.CursorLoader cursorLoader = new androidx.loader.content.CursorLoader(this, FinanceContentProvider.ENDERECO_CATEGORIAS, BdTabelaCategoria.TODAS_COLUNAS, null, null, BdTabelaCategoria.CAMPO_DESCRICAO
+        );
+
+        return cursorLoader;
+    }
+
+    @Override
+    public void onLoadFinished(@NonNull Loader<Cursor> loader, Cursor data) {
+        mostraCategoriasSpinner(data);
+        categoriasCarregadas = true;
+        actualizaCategoriaSelecionada();
+    }
+
+    @Override
+    public void onLoaderReset(@NonNull Loader<Cursor> loader) {
+        categoriasCarregadas = false;
+        categoriaAtualizada = false;
+        mostraCategoriasSpinner(null);
+    }
 }

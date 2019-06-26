54734530eadb86a8780c979747f2beb6cd395b13
Vagner Bom Jesus <vagneripg@gmail.com>
2019-06-26 23:26:10 +0100

    Atualizacao internacionalisacao

--- app/src/main/java/pt/vagner/myfinances/EditDespesa.java
+++ app/src/main/java/pt/vagner/myfinances/EditDespesa.java
@@ -124,7 +124,7 @@ public class EditDespesa extends AppCompatActivity implements DialogFragmentCate
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
+       // getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
 
